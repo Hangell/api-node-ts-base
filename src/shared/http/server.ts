bfaeca28268f7481d299f5d969e55288cc09afd4
Rodrigo Rangel <rodrigo@hangell.org>
2021-09-22 19:28:52 -0300

    Server and Routes, Cors and Express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,15 @@
-console.log('Hello Dev!');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+const PORT = 3333;
+app.listen(PORT, () => {
+  console.log(`Server Started on port ${PORT}!`);
+});
